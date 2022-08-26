faada063f1bb86f147c6a9df9b561ff7f6549997
joao.junior.ext@sascar.com.br <joao.junior.ext@sascar.com.br>
2022-08-26 00:16:29 -0300

    finish

---
+++ src/main/resources/dump-dont_use-automatic_create_db.sql
@@ -0,0 +1,108 @@
+-- --------------------------------------------------------
+-- Servidor:                     127.0.0.1
+-- Versão do servidor:           10.6.4-MariaDB - mariadb.org binary distribution
+-- OS do Servidor:               Win64
+-- HeidiSQL Versão:              11.3.0.6295
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+-- Copiando estrutura do banco de dados para atividade07
+CREATE DATABASE IF NOT EXISTS `atividade07` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `atividade07`;
+
+-- Copiando estrutura para tabela atividade07.departamento
+CREATE TABLE IF NOT EXISTS `departamento` (
+  `id` bigint(20) NOT NULL,
+  `nome_departamento` varchar(255) DEFAULT NULL,
+  `id_empresa` bigint(20) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FKjaqx7ovf0qq096a1ya2b7cc2x` (`id_empresa`),
+  CONSTRAINT `FKjaqx7ovf0qq096a1ya2b7cc2x` FOREIGN KEY (`id_empresa`) REFERENCES `empresa` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Copiando dados para a tabela atividade07.departamento: ~4 rows (aproximadamente)
+/*!40000 ALTER TABLE `departamento` DISABLE KEYS */;
+INSERT INTO `departamento` (`id`, `nome_departamento`, `id_empresa`) VALUES
+	(5, 'Departamento 01', 2),
+	(6, 'Departamento 02', 2),
+	(7, 'Departamento 03', 3),
+	(9, 'Departamento 05', 4);
+/*!40000 ALTER TABLE `departamento` ENABLE KEYS */;
+
+-- Copiando estrutura para tabela atividade07.empresa
+CREATE TABLE IF NOT EXISTS `empresa` (
+  `id` bigint(20) NOT NULL,
+  `nome_empresa` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Copiando dados para a tabela atividade07.empresa: ~4 rows (aproximadamente)
+/*!40000 ALTER TABLE `empresa` DISABLE KEYS */;
+INSERT INTO `empresa` (`id`, `nome_empresa`) VALUES
+	(2, 'Empresa 02'),
+	(3, 'Empresa 03'),
+	(4, 'Empresa 04');
+/*!40000 ALTER TABLE `empresa` ENABLE KEYS */;
+
+-- Copiando estrutura para tabela atividade07.funcionario
+CREATE TABLE IF NOT EXISTS `funcionario` (
+  `id` bigint(20) NOT NULL,
+  `cargo` varchar(255) DEFAULT NULL,
+  `cpf` varchar(255) NOT NULL,
+  `nome_funcionario` varchar(255) DEFAULT NULL,
+  `qtde_dependente` int(11) DEFAULT NULL,
+  `salario` double DEFAULT NULL,
+  `id_departamento` bigint(20) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK4362eulyqqf15ih924arflvmi` (`id_departamento`),
+  CONSTRAINT `FK4362eulyqqf15ih924arflvmi` FOREIGN KEY (`id_departamento`) REFERENCES `departamento` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Copiando dados para a tabela atividade07.funcionario: ~4 rows (aproximadamente)
+/*!40000 ALTER TABLE `funcionario` DISABLE KEYS */;
+INSERT INTO `funcionario` (`id`, `cargo`, `cpf`, `nome_funcionario`, `qtde_dependente`, `salario`, `id_departamento`) VALUES
+	(10, 'Cargo 01', '04252308', 'Funcionario 01', 0, 27639787520, 7),
+	(12, 'Cargo 03', '042523083', 'Funcionario 03', 3, 27639787520, 9),
+	(13, 'Cargo 04', '042523084', 'Funcionario 04', 4, 27639787520, 9),
+	(14, 'Cargo 05', '042523085', 'Funcionario 05', 5, 27639787520, 7);
+/*!40000 ALTER TABLE `funcionario` ENABLE KEYS */;
+
+-- Copiando estrutura para tabela atividade07.hibernate_sequence
+CREATE TABLE IF NOT EXISTS `hibernate_sequence` (
+  `next_not_cached_value` bigint(21) NOT NULL,
+  `minimum_value` bigint(21) NOT NULL,
+  `maximum_value` bigint(21) NOT NULL,
+  `start_value` bigint(21) NOT NULL COMMENT 'start value when sequences is created or value if RESTART is used',
+  `increment` bigint(21) NOT NULL COMMENT 'increment value',
+  `cache_size` bigint(21) unsigned NOT NULL,
+  `cycle_option` tinyint(1) unsigned NOT NULL COMMENT '0 if no cycles are allowed, 1 if the sequence should begin a new cycle when maximum_value is passed',
+  `cycle_count` bigint(21) NOT NULL COMMENT 'How many cycles have been done'
+) ENGINE=InnoDB SEQUENCE=1;
+
+-- Copiando dados para a tabela atividade07.hibernate_sequence: ~1 rows (aproximadamente)
+/*!40000 ALTER TABLE `hibernate_sequence` DISABLE KEYS */;
+INSERT INTO `hibernate_sequence` (`next_not_cached_value`, `minimum_value`, `maximum_value`, `start_value`, `increment`, `cache_size`, `cycle_option`, `cycle_count`) VALUES
+	(1001, 1, 9223372036854775806, 1, 1, 1000, 0, 0);
+/*!40000 ALTER TABLE `hibernate_sequence` ENABLE KEYS */;
+
+-- Copiando estrutura para procedure atividade07.proc_aumentaSalario
+DELIMITER //
+CREATE PROCEDURE `proc_aumentaSalario`(IN in_id FLOAT)
+    READS SQL DATA
+BEGIN 
+   UPDATE funcionario f SET f.salario = salario + (salario * (in_id/100));
+   SELECT * FROM funcionario f;
+END//
+DELIMITER ;
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
