cceecffb32fbc519550b63117644070077870eb9
joao.junior.ext@sascar.com.br <joao.junior.ext@sascar.com.br>
2022-09-04 17:30:56 -0300

    refactor metodos

--- src/main/java/com/simulado/ApplicationCommandLineRunner.java
+++ src/main/java/com/simulado/ApplicationCommandLineRunner.java
@@ -4,6 +4,7 @@ import java.util.Date;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -16,6 +17,7 @@ import com.simulado.entity.Gravacao;
 import com.simulado.entity.Gravadora;
 import com.simulado.entity.Musica;
 import com.simulado.entity.Pessoa;
+import com.simulado.exception.OperationException;
 import com.simulado.service.CantorService;
 import com.simulado.service.CategoriaService;
 import com.simulado.service.FoneService;
@@ -28,111 +30,44 @@ import com.simulado.service.PessoaService;
 public class ApplicationCommandLineRunner {
 	
 	private static final Logger log = LoggerFactory.getLogger(ApplicationCommandLineRunner.class);
+	
+	@Autowired
+	PessoaService pessoaService;
+	
+	@Autowired
+	FoneService foneService;
+	
+	@Autowired
+	CategoriaService categoriaService;
+	
+	@Autowired
+	MusicaService musicaService;
+	
+	@Autowired
+	GravadoraService gravadoraService;	
+	
+	@Autowired
+	CantorService cantorService;
+	
+	@Autowired
+	GravacaoService gravacaoService;
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApplicationCommandLineRunner.class, args);
 	}
 
 	@Bean
-	public CommandLineRunner demo(
-					PessoaService pessoaService,
-					FoneService foneService,
-					CategoriaService categoriaService,
-					MusicaService musicaService,
-					GravadoraService gravadoraService,	
-					CantorService cantorService,
-					GravacaoService gravacaoService
-			) {
+	public CommandLineRunner demo() {
+		
 		return (args) -> {
 			
-			//SAVEs
-			
-			// salvando algumas pessoas
-			pessoaService.save(new Pessoa("nome pessoa 01"));
-			pessoaService.save(new Pessoa("nome pessoa 02"));
-			pessoaService.save(new Pessoa("nome pessoa 03"));
-			
-			// salvando alguns fones
-			foneService.save(new Fone("41 9999-9999", "C", pessoaService.getFirst()));
-			foneService.save(new Fone("43 8888-8888", "R", pessoaService.getLast()));
-			
-			// salvando algumas categorias
-			categoriaService.save(new Categoria("categoria 01"));
-			categoriaService.save(new Categoria("categoria 02"));			
-			
-			// salvando algumas musicas
-			musicaService.save(new Musica("titulo 01", 10, categoriaService.getFirst()));
-			musicaService.save(new Musica("titulo 02", 12, categoriaService.getLast()));
-			
-			// salvando alguns cantores
-			cantorService.save(new Cantor("nome cantor 01", "USA"));
-			cantorService.save(new Cantor("nome cantor 02", "BRASIL"));
-			cantorService.save(new Cantor("nome cantor 03", "JAPAO"));
-			
-			// salvando algumas gravadoras
-			gravadoraService.save(new Gravadora("nome gravadora 01", "USA"));
-			gravadoraService.save(new Gravadora("nome gravadora 02", "BRASIL"));
-			gravadoraService.save(new Gravadora("nome gravadora 03", "JAPAO"));
-			
-			// salvando algumas gravacoes
-			gravacaoService.save(new Gravacao(new Date(), 
-											  musicaService.getFirst(), 
-											  cantorService.getFirst(), 
-											  gravadoraService.getFirst()));			
-			gravacaoService.save(new Gravacao(new Date(), 
-											  musicaService.getLast(), 
-											  cantorService.getLast(), 
-											  gravadoraService.getLast()));
+			//SAVEs			
+			saves();
 			
 			// SELECTs
-			log.info("-----------------------------------------------------------------------");
-			
-			//retornando dados - Pessoa
-			log.info("-----------------------------------------------------------------------");
-			log.info("retornando dados - Pessoa");
-			log.info("-----------------------------------------------------------------------");
-			
-			log.info("-----------------------------------------------------------------------");
-			
-			log.info("Pessoas encontradas com findAll():");
-			log.info("-------------------------------");
-			for (Pessoa pessoa : pessoaService.getAll()) {
-				log.info(pessoa.toString());
-			}
-			log.info("");
-			
-			log.info("Pessoa encontrada com getAllByQuery():");
-			log.info("-------------------------------");			
-			for (Pessoa pessoa : pessoaService.getAllByQuery()) {
-				log.info(pessoa.toString());
-			}			
-			log.info("");
-			
-			log.info("-----------------------------------------------------------------------");			
-			
-			
-			//retornando dados - Fone
-			log.info("-----------------------------------------------------------------------");
-			log.info("retornando dados - Fone");
-			log.info("-----------------------------------------------------------------------");
-			
-			log.info("-----------------------------------------------------------------------");
-			
-			log.info("Fones encontrados com findAll():");
-			log.info("-------------------------------");
-			for (Fone fones : foneService.getAll()) {
-				log.info(fones.toString());
-			}
-			log.info("");
-			
-			log.info("Fone encontrado com getByNumero():");
-			log.info("-------------------------------");			
-			
-			log.info(foneService.getByNumero("41 9999-9999").toString());
-						
-			log.info("");
-			
-			log.info("-----------------------------------------------------------------------");
+			selectsPessoa();
+			selectsFone();
 			
 			
 			
@@ -157,4 +92,90 @@ public class ApplicationCommandLineRunner {
 			
 		};
 	}
+	
+	private void saves() throws OperationException {
+		
+		// salvando algumas pessoas
+		pessoaService.save(new Pessoa("nome pessoa 01"));
+		pessoaService.save(new Pessoa("nome pessoa 02"));
+		pessoaService.save(new Pessoa("nome pessoa 03"));
+		
+		// salvando alguns fones
+		foneService.save(new Fone("41 9999-9999", "C", pessoaService.getFirst()));
+		foneService.save(new Fone("43 8888-8888", "R", pessoaService.getLast()));
+		
+		// salvando algumas categorias
+		categoriaService.save(new Categoria("categoria 01"));
+		categoriaService.save(new Categoria("categoria 02"));			
+		
+		// salvando algumas musicas
+		musicaService.save(new Musica("titulo 01", 10, categoriaService.getFirst()));
+		musicaService.save(new Musica("titulo 02", 12, categoriaService.getLast()));
+		
+		// salvando alguns cantores
+		cantorService.save(new Cantor("nome cantor 01", "USA"));
+		cantorService.save(new Cantor("nome cantor 02", "BRASIL"));
+		cantorService.save(new Cantor("nome cantor 03", "JAPAO"));
+		
+		// salvando algumas gravadoras
+		gravadoraService.save(new Gravadora("nome gravadora 01", "USA"));
+		gravadoraService.save(new Gravadora("nome gravadora 02", "BRASIL"));
+		gravadoraService.save(new Gravadora("nome gravadora 03", "JAPAO"));
+		
+		// salvando algumas gravacoes
+		gravacaoService.save(new Gravacao(new Date(), 
+										  musicaService.getFirst(), 
+										  cantorService.getFirst(), 
+										  gravadoraService.getFirst()));			
+		gravacaoService.save(new Gravacao(new Date(), 
+										  musicaService.getLast(), 
+										  cantorService.getLast(), 
+										  gravadoraService.getLast()));
+	}	
+
+	private void selectsPessoa() throws OperationException {
+		log.info("-----------------------------------------------------------------------");
+		
+		//retornando dados - Pessoa
+		log.info("-----------------------------------------------------------------------");
+		log.info("retornando dados - Pessoa");
+		log.info("-----------------------------------------------------------------------");			
+		log.info("-----------------------------------------------------------------------");
+		log.info("");
+		log.info("Pessoas encontradas com findAll():");
+		log.info("-------------------------------");
+			for (Pessoa pessoa : pessoaService.getAll()) {
+				log.info(pessoa.toString());
+			}
+		log.info("");			
+		log.info("Pessoa encontrada com getAllByQuery():");
+		log.info("-------------------------------");			
+			for (Pessoa pessoa : pessoaService.getAllByQuery()) {
+				log.info(pessoa.toString());
+			}			
+		log.info("");			
+		log.info("-----------------------------------------------------------------------");
+	}
+	
+	private void selectsFone() throws OperationException {
+		//retornando dados - Fone
+		log.info("-----------------------------------------------------------------------");
+		log.info("retornando dados - Fone");
+		log.info("-----------------------------------------------------------------------");			
+		log.info("-----------------------------------------------------------------------");
+		log.info("");
+		log.info("Fones encontrados com findAll():");
+		log.info("-------------------------------");
+			for (Fone fones : foneService.getAll()) {
+				log.info(fones.toString());
+			}
+		log.info("");			
+		log.info("Fone encontrado com getByNumero():");
+		log.info("-------------------------------");			
+			log.info(foneService.getByNumero("41 9999-9999").toString());
+		log.info("");			
+		log.info("-----------------------------------------------------------------------");
+	}
+
+	
 }
