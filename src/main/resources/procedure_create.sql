c55fea6d73b5d47aaa26bec1c3a6d4a9c6b084b9
joao.junior.ext@sascar.com.br <joao.junior.ext@sascar.com.br>
2022-09-04 01:29:07 -0300

    exclude .sqls

--- src/main/resources/procedure_create.sql
+++
@@ -1,8 +0,0 @@
-DELIMITER && 
-CREATE PROCEDURE proc_aumentaSalario(IN in_id FLOAT)
-READS SQL DATA
-BEGIN 
-   UPDATE funcionario f SET f.salario = salario + (salario * (in_id/100));
-   SELECT * FROM funcionario f;
-END &&  
-DELIMITER ;
